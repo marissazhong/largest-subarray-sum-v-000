681aa0514c8d79c0b6acc18bf4a5c11f647c5143
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 16:52:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function largestSubarraySum(array) {
   // 1. find the largest subarray sum starting from index 0
   let sum_array = [array[1]];
   for (let i = 1; i < array.length; i++) {
-    sum_array.push(array[i-1]+array[i]);
+    sum_array.push(sum_array[i-1]+array[i]);
   }
   console.log(sum_array);
 }
